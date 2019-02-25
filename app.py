1019197cde99dad929a9fe7b1909829131368485
Sneha Singhi <singhi.sneha98@gmail.com>
2019-02-25 09:15:10 +0530

    Added login template and email sending code

--- app.py
+++ app.py
@@ -12,6 +12,14 @@ app.config['MYSQL_DATABASE_DB'] = 'Email'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
 
+"""app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'yourId@gmail.com'
+app.config['MAIL_PASSWORD'] = '*****'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)"""
+
 app = Flask(__name__)
 @app.route("/")
 def main():
@@ -24,6 +32,21 @@ def showSignUp():
 @app.route('/showCompose')
 def showCompose():
     return render_template('compose.html')
+
+@app.route('/showInbox')
+def showInbox():
+    return render_template('inbox.html')
+
+
+""" @app.route("/")
+def index():
+   msg = Message('Hello', sender = 'yourId@gmail.com', recipients = ['id1@gmail.com'])
+   msg.body = "Hello Flask message sent from Flask-Mail"
+   mail.send(msg)
+   return "Sent"
+
+if __name__ == '__main__':
+   app.run(debug = True) """
 
 if __name__ == "__main__":
     app.run()
