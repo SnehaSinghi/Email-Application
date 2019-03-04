03aac1eb563e7d246e71caacf01d8927e14e5a89
Sneha Singhi <singhi.sneha98@gmail.com>
2019-03-04 10:45:20 +0530

    Backend done

---
+++ Test/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'c03bcfa7c0f29d2192ac0b84991c693f'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+from Test import routes
